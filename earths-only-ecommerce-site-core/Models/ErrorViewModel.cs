99c5b12a2656a33fb60a4ba0a46a315f02e88493
Lucas Fredin <ii.fredin@gmail.com>
2020-01-17 15:39:01 +0100

    first basic

---
+++ earths-only-ecommerce-site-core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace earths_only_ecommerce_site_core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ earths-only-ecommerce-site-core/Models/Poco/ItemForSale.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace earths_only_ecommerce_site_core.Models.Poco
+{
+    public class ItemForSale
+    {
+    }
+}
