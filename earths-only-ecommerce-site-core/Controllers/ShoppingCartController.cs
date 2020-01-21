ff6c3bd5d45e6c8a455d48427566239a37b4589d
Lucas Fredin <ii.fredin@gmail.com>
2020-01-21 15:53:23 +0100

    setting up cart

---
+++ earths-only-ecommerce-site-core/Controllers/ShoppingCartController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace earths_only_ecommerce_site_core.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cart(string itemId)
+        {
+            return View();
+        }
+
+    }
+}
